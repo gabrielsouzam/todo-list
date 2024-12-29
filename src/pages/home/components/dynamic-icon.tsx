764822bdaa143a1a2d84a6572a1a07bcfed89d32
gabrielmendes64 <gabriel10010100@gmail.com>
2024-12-29 10:23:31 -0300

    feat: add reusable component DynamicIcon

---
+++ src/pages/home/components/dynamic-icon.tsx
@@ -0,0 +1,22 @@
+import { iconMap } from "@/utils/iconMap";
+import React from "react";
+
+interface DynamicIconProps {
+  icon: string;
+  color?: string;
+  size?: number;
+};
+
+export const DynamicIcon: React.FC<DynamicIconProps> = ({
+  icon,
+  color = "black",
+  size = 24,
+}) => {
+  const IconComponent = iconMap[icon]; 
+  if (!IconComponent) {
+    console.warn(`Icon "${icon}" not found in iconMap.`);
+    return null; 
+  }
+
+  return <IconComponent size={size} color={color} weight="fill" />;
+};
