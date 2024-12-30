a602eda4be9c7fe8f7193e8ae33128d369d2ac64
gabrielmendes64 <gabriel10010100@gmail.com>
2024-12-30 18:23:04 -0300

    chore: add new dependencie date-fns

--- src/pages/home/components/select-priority.tsx
+++ src/pages/home/components/select-priority.tsx
@@ -5,21 +5,40 @@ import {
   SelectItem,
   SelectRoot,
   SelectTrigger,
-  SelectValueText
+  SelectValueText,
 } from "@chakra-ui/react";
 
 const priority = createListCollection({
   items: [
-    { label: "Baixa", value: "baixa" },
-    { label: "Média", value: "media" },
-    { label: "Alta", value: "alta" },
+    { label: "Todas", value: "" },
+    { label: "Baixa", value: "low" },
+    { label: "Média", value: "medium" },
+    { label: "Alta", value: "high" },
   ],
 });
 
-export function SelectPriority() {
+interface SelectPriorityProps {
+  onSelect: (value: string) => void;
+}
+
+export function SelectPriority({ onSelect }: SelectPriorityProps) {
   return (
-    <SelectRoot collection={priority} size="lg" width="20rem" style={{ position: "relative" }}>
-      <SelectTrigger cursor="pointer" height="1rem"  >
+    <SelectRoot
+      collection={priority}
+      size="lg"
+      width="20rem"
+      onValueChange={({ value }) => onSelect(value[0])}
+      style={{ position: "relative"}}
+    >
+      <SelectTrigger
+        cursor="pointer"
+        height="3rem"
+        bg="gray.950"
+        borderRadius="md"
+        border="1px solid"
+        borderColor="gray.800"
+        _hover={{ borderColor: "gray.700" }}
+      >
         <SelectValueText placeholder="Filtre por prioridade" />
         <CaretDown size={16} />
       </SelectTrigger>
@@ -36,7 +55,7 @@ export function SelectPriority() {
         width="100%"
       >
         {priority.items.map((priority) => (
-          <SelectItem item={priority} key={priority.value} cursor="pointer">
+          <SelectItem item={priority} key={priority.value} cursor="pointer" _hover={{ bg: "gray.700" }}>
             {priority.label}
           </SelectItem>
         ))}
